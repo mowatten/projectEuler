d22767cbcf06ea9887e219a41b38750bf93fccdc
thomas <thomas@0174-E7440.Enivest.local>
2015-03-18 18:09:32 +0100

    Finpuss

--- Problem_1/src/problem_1.py
+++ Problem_1/src/problem_1.py
@@ -5,16 +5,21 @@
 __author__ = "thomas"
 __date__ = "$18.mar.2015 17:28:22$"
 
+#START
+
 import time
 
 if __name__ == "__main__":
     start = time.time()
     
-    s = sum(set(range(0,1000,3))|set(range(0,1000,5)))
+    resultat = sum(set(range(0,1000,3))|set(range(0,1000,5)))
 
-    elapsed = time.time() - start
+    tid = time.time() - start
     
-    print("result %s returned in %s seconds" % (s,elapsed))
+    print("result %s returned in %s seconds" % (resultat,tid))
+    
+#END      
+
 #1 2 3 4 5 6 7 8 9
 #    3   5 6     9
 
