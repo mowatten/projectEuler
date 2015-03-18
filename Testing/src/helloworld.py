3c7240dd3e05184b9c094115a2386199cc4cf435
thomas <thomas@0174-E7440.Enivest.local>
2015-03-18 17:21:12 +0100

    Endra mappenamn

---
+++ Testing/src/helloworld.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "thomas"
+__date__ = "$18.mar.2015 17:18:56$"
+
+if __name__ == "__main__":
+    print ("Hello World")
