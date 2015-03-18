e44e529a000b22a93cdc6374f6621b46c004ec3a
thomas <thomas@0174-E7440.Enivest.local>
2015-03-18 17:58:58 +0100

    Lagt med timer.

--- Problem_1/src/problem_1.py
+++ Problem_1/src/problem_1.py
@@ -5,10 +5,16 @@
 __author__ = "thomas"
 __date__ = "$18.mar.2015 17:28:22$"
 
+import time
+
 if __name__ == "__main__":
+    start = time.time()
     
-    print(sum(set(range(0,1000,3))|set(range(0,1000,5))))
+    s = sum(set(range(0,1000,3))|set(range(0,1000,5)))
+
+    elapsed = time.time() - start
     
+    print("result %s returned in %s seconds" % (s,elapsed))
 #1 2 3 4 5 6 7 8 9
 #    3   5 6     9
 
